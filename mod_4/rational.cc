eaab5cba7f52917509bb95f9849bdd959bd44499
William Long <wl359@cornell.edu>
2018-10-09 23:07:50 -0400

    implemented postfix calculator

--- mod_4/rational.cc
+++ mod_4/rational.cc
@@ -1,5 +1,6 @@
 #include "rational.h"
 #include <iostream>
+#include <stack>
 
 Rational::Rational(int a, int b) : num(a), den(b) {}
 
@@ -56,24 +57,67 @@ bool operator==(const Rational& r1, const Rational& r2) {
   return r1.value() == r2.value();
 }
 
+double postfix(std::string s) {
+  std::stack<Rational> stack;
+  bool end = false;
+  while(!end) {
+    int next = s.find(" ");
+    if(next == std::string::npos) {
+      next = s.size();
+      end = true;
+    }
+    std::string op = s.substr(0, next);
+    if(!end)
+      s = s.substr(next + 1);
+    if(op.size() > 1) {
+      int bar = op.find("/");
+      std::string num = op.substr(0, bar);
+      op = op.substr(bar);
+      std::string den = op.substr(1);
+      int numerator = std::stoi(num);
+      int denominator = std::stoi(den);
+      stack.push(Rational(numerator, denominator));
+    } else {
+      Rational number2 = stack.top();
+      stack.pop();
+      Rational number1 = stack.top();
+      stack.pop();
+      if(op == "+") {
+        stack.push(number1 + number2);
+      } else if (op == "-") {
+        stack.push(number1 - number2);
+      } else if (op == "*") {
+        stack.push(number1 * number2);
+      } else if (op == "/") {
+        stack.push(number1 / number2);
+      }
+    }
+  }
+  return stack.top().value();
+}
+
 int main() {
-  Rational one_half(1,2);
-  Rational one_third(1,3);
-  Rational two_thirds(2,3);
-  Rational sum = one_half + two_thirds;
-  Rational minus = one_half - two_thirds;
-  Rational product = one_half * two_thirds;
-  Rational divide = one_half / two_thirds;
-  bool e = one_half == one_half;
-  bool n = one_half == two_thirds;
-  std::cout << one_half.value() << std::endl;
-  std::cout << one_third.value() << std::endl;
-  std::cout << two_thirds.value() << std::endl;
-  std::cout << sum.value() << std::endl;
-  std::cout << minus.value() << std::endl;
-  std::cout << product.value() << std::endl;
-  std::cout << divide.value() << std::endl;
-  std::cout << e << std::endl;
-  std::cout << n << std::endl;
+  // Rational one_half(1,2);
+  // Rational one_third(1,3);
+  // Rational two_thirds(2,3);
+  // Rational sum = one_half + two_thirds;
+  // Rational minus = one_half - two_thirds;
+  // Rational product = one_half * two_thirds;
+  // Rational divide = one_half / two_thirds;
+  // bool e = one_half == one_half;
+  // bool n = one_half == two_thirds;
+  // std::cout << one_half.value() << std::endl;
+  // std::cout << one_third.value() << std::endl;
+  // std::cout << two_thirds.value() << std::endl;
+  // std::cout << sum.value() << std::endl;
+  // std::cout << minus.value() << std::endl;
+  // std::cout << product.value() << std::endl;
+  // std::cout << divide.value() << std::endl;
+  // std::cout << e << std::endl;
+  // std::cout << n << std::endl;
+  std::string s;
+  std::cout << "Please enter your postfix expression: ";
+  std::getline(std::cin, s);
+  std::cout << postfix(s) << std::endl;
   return 0;
 }
